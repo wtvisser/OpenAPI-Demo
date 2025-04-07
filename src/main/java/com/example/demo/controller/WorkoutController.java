12b4dcddb1df93c05e7d6d059cd71553d61a1182
Wicher Visser <wtvisser@gmail.com>
2025-04-07 12:46:31 +0200

    Create WorkoutController.java

---
+++ src/main/java/com/example/demo/controller/WorkoutController.java
@@ -0,0 +1,8 @@
+package com.baeldung.apifirstdevelopment.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WorkoutController {
+}
